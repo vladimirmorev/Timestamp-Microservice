f014fa88e9cdc2b83add2c7d15d2c3c0dbe5e744
vladimirmorev <v.morev90@gmail.com>
2016-02-19 16:36:32 +0600

    setup body-parser

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+// Config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
